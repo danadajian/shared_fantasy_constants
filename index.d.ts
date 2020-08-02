b1d481c22a7e3584690866c89490bee08717d01c
Dan Adajian <danadajian@gmail.com>
2020-08-02 13:09:02 -0500

    * remove aws stuff

--- index.d.ts
+++ index.d.ts
@@ -4,8 +4,3 @@ export declare const SUPPORTED_CONTESTS: string[];
 export declare const LINEUP_RULES: any;
 export declare const DFS_PIPELINE_BUCKET_NAME: string;
 export declare const FANTASY_ANALYTICS_BUCKET_NAME: string;
-export declare function retrieveObjectFromS3 (bucketName: string, fileName: string): Promise<any>;
-export declare function uploadObjectToS3 (object: any, bucketName: string, fileName: string): Promise<any>;
-export declare function publishToSnsTopic (message: string, topicArn: string): Promise<any>;
-export declare function createCloudWatchEvent (putRuleParams: any, putTargetsParams: any): Promise<any>;
-export declare function invokeLambdaFunction (functionName: string, payload: any): Promise<any>;
--- scripts/createEnvironmentVariables.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-{
-  echo "AWS_KEY=$AWS_ACCESS_KEY_ID"
-  echo "AWS_SECRET=$AWS_SECRET_ACCESS_KEY"
-} >> ./.env
-
-cat ./.env
